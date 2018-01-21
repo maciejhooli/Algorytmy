37f389835d926018535a37b83f163d984d0f756c
Unknown <maciejholysz@gmail.com>
2018-01-21 14:48:40 +0100

    Gra w 100
    Początek prac

---
+++ src/Projekty/OneHundredCounting/Game.java
@@ -0,0 +1,8 @@
+package Projekty.OneHundredCounting;
+
+public class Game {
+    public static void main(String[] args) {
+        OneHundredGame game = new OneHundredGame();
+        game.startGame();
+    }
+}
---
+++ src/Projekty/OneHundredCounting/OneHundredGame.java
@@ -0,0 +1,15 @@
+package Projekty.OneHundredCounting;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class OneHundredGame {
+    private int actualresult = 100;
+
+    public static void startGame(){
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int userChoice
+    }
+}
