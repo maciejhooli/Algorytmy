3a2de156e14228aca7b6329c26d5b32c05fd60d1
Unknown <maciejholysz@gmail.com>
2018-01-21 15:18:03 +0100

    Gra w 100
    Wykonanie wersji losowej

--- src/Projekty/OneHundredCounting/OneHundredGame.java
+++ src/Projekty/OneHundredCounting/OneHundredGame.java
@@ -1,15 +1,49 @@
 package Projekty.OneHundredCounting;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class OneHundredGame {
     private int actualresult = 100;
-
-    public static void startGame(){
+    public void startGame() {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
+        int userChoice = 0;
+        int compChoice = 0;
+        System.out.println("Witaj w grze w 100");
+        while (actualresult >= 1) {
+            System.out.println("Podaj wartość między 1-10.");
+            try {
+                userChoice = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Złe dane!! Podaj jeszcze raz");
+                sc.next();
+                continue;
+            }
+            actualresult -= userChoice;
+            System.out.println("Actual result after user: " + actualresult);
+            if (gameEnd()){
+                System.out.println("User WIN!!");
+                break;
+            }
+            if(actualresult>10) {
+                compChoice = rand.nextInt(10)+1;
+            }
+            else {
+                compChoice = rand.nextInt(actualresult)+1;
+            }
+            System.out.println("Comp choice: " + compChoice);
+            actualresult-=compChoice;
+            System.out.println("Actual result after comp: " + actualresult);
 
-        int userChoice
+            if(gameEnd()){
+                System.out.println("Comp WIN");
+            }
+        }
+    }
+
+    private boolean gameEnd(){
+        return (actualresult<=1);
     }
 }
